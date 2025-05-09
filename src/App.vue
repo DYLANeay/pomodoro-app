1b0655a7b14119b54d10c5105294cebe8f844560
DYLANeay <dylan.eray6@gmail.com>
2025-05-09 18:59:17 +0200

    Quote components, call to API and basic styling done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <h1 class="text-center">Hello World</h1>
+  <h1 class="text-center welcomeText">Welcome Home</h1>
   <Quote />
 </template>
 
 <script setup>
 // import { ref } from 'vue'
-import Quote from './components/Quote.vue'
+import Quote from './components/Quote.vue';
 </script>
 
 <style>
 @import 'tailwindcss';
+@import '@/assets/styles/global.css';
+
+.welcomeText {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
 </style>
--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,8 +1,48 @@
 <template>
-  <p>Hi</p>
+  <div class="quote center">
+    <p class="text-center">{{ quote }}</p>
+    <p class="author">
+      <strong
+        ><i>- {{ author }}</i></strong
+      >
+    </p>
+  </div>
 </template>
 <script setup>
+import { onMounted, ref } from 'vue';
+
 defineOptions({
   name: 'MotivationalQuote',
-})
+});
+
+let quote = ref('');
+let author = ref('');
+
+//get the quotes and authors from the API
+onMounted(async () => {
+  const response = await fetch(
+    'https://raw.githubusercontent.com/AtaGowani/daily-motivation/refs/heads/master/src/data/quotes.json',
+  );
+
+  const data = await response.json();
+  const randomIndex = Math.floor(Math.random() * data.length); // get a random index
+  // get a random quote and author
+  quote.value = data[randomIndex].quote;
+  author.value = data[randomIndex].author;
+});
 </script>
+
+<style>
+.quote {
+  padding: 0.5rem;
+  margin: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+}
+
+.author {
+  font-size: 0.5rem;
+  font-weight: light;
+  justify-self: right;
+}
+</style>
